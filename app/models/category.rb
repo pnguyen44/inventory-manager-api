12fcaceb622142fcd408a6f2ae1c84550334cb42
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-01 14:09:47 -0400

    Update category model   -remove dependent: :destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :items, dependent: :destroy
+  has_many :items
   # validates :name, :user, presence: true
   validates_uniqueness_of :name, scope: :user_id, presence: true
 end

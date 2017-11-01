3ca0153558977ee451707d29ea859c25f87c732b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-01 14:50:54 -0400

    Ran migration

--- db/migrate/20171101181655_create_items.rb
+++ db/migrate/20171101181655_create_items.rb
@@ -3,9 +3,9 @@ class CreateItems < ActiveRecord::Migration[5.1]
     create_table :items do |t|
       t.string :name
       t.string :description
-      t.number :currentQuanity
-      t.number :alertQuanity
-      t.number :quanityPurchased
+      t.integer :currentQuanity
+      t.integer :alertQuanity
+      t.integer :quanityPurchased
       t.boolean :inShoppingList
       t.references :category, index: true, foreign_key: true, null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101150840) do
+ActiveRecord::Schema.define(version: 20171101181655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 20171101150840) do
     t.index ["user_id"], name: "index_examples_on_user_id"
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "currentQuanity"
+    t.integer "alertQuanity"
+    t.integer "quanityPurchased"
+    t.boolean "inShoppingList"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "token", null: false
@@ -43,4 +56,5 @@ ActiveRecord::Schema.define(version: 20171101150840) do
 
   add_foreign_key "categories", "users"
   add_foreign_key "examples", "users"
+  add_foreign_key "items", "categories"
 end

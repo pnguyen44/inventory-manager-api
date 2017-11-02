7fbbe9494d9b4014ddc509e624d0449f9f57f927
pnguyen44 <nguyen.phuongnhat@gmail.com>
2017-11-01 20:48:33 -0400

    Update item model   - remove required presence of currentQuantity, alertQuantity, inOrderList, and quantityPurchased attributes.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :category
   has_one :user, through: :category
-  validates :name, :currentQuantity, :alertQuantity, :quantityPurchased, presence: true
+  validates :name, presence: true
 end
